7936dc3a6a15f5213a6ecc4c7d11a996110531ad
jsmolar <410313@mail.muni.cz>
2017-06-18 20:33:32 +0200

    addet simple view

--- src/main/java/Controllers/WeatherController.java
+++ src/main/java/Controllers/WeatherController.java
@@ -2,6 +2,7 @@ package Controllers;
 
 import Entity.Weather;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,20 +17,31 @@ public class WeatherController {
     private Weather weather = null;
 
     @RequestMapping(method = RequestMethod.GET)
-    public String getWeather(){
+    public String getWeather(Model model){
         if(weather == null){
+            model.addAttribute("temperature", "NA");
+            model.addAttribute("humidity", "NA");
+            model.addAttribute("description", "NA");
+
             return "weather";
         }
 
-        //return "Temperature: " + String.valueOf(weather.getTemperature()) + "°C \n Humidity: " + weather.getHumidity() + "% \n Description: " + weather.getDescription();
+        model.addAttribute("temperature", String.valueOf(weather.getTemperature()));
+        model.addAttribute("humidity", weather.getHumidity());
+        model.addAttribute("description", weather.getDescription());
+//        return "Temperature: " + String.valueOf(weather.getTemperature()) + "°C \n Humidity: " + weather.getHumidity() + "% \n Description: " + weather.getDescription();
         return "weather";
     }
 
     @RequestMapping(value = "/sendValue", method = RequestMethod.POST)
-    public String setWeather(@RequestBody Weather input){
+    public String setWeather(@RequestBody Weather input, Model model){
         weather = input;
 
-        return "Sucess " + weather;
+        model.addAttribute("temperature", String.valueOf(weather.getTemperature()));
+        model.addAttribute("humidity", weather.getHumidity());
+        model.addAttribute("description", weather.getDescription());
+        //        return "Temperature: " + String.valueOf(weather.getTemperature()) + "°C \n Humidity: " + weather.getHumidity() + "% \n Description: " + weather.getDescription();
+        return "weather";
     }
 
 
